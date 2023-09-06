a399fe7440ecf5cbe47413fb530086f36969acc9
predeludel <sinitsa.2003@yandex.ru>
2023-09-06 22:14:52 +0300

    task rock paper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,16 @@
+first_player, second_player = str(input("Введите два слова:")).split(" ")
+
+words = ["ничья", "камень", "ножницы", "бумага"]
+
+
+def find_winner(first, second):
+    summ = words.index(first) - words.index(second)
+    if -2 < summ < 0 or summ == 2:
+        return "игрок 1 выиграл"
+    elif 2 > summ > 0 or summ == -2:
+        return "игрок 2 выиграл"
+    else:
+        return words[0]
+
+
+print(find_winner(first_player, second_player))
