6e25b9fccb23b7b0dd7ed7c6f9965fe821a3f3be
predeludel <sinitsa.2003@yandex.ru>
2023-09-07 22:18:29 +0300

    edits

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,18 @@
 first_player, second_player = str(input("Введите два слова:")).split(" ")
 
-words = ["ничья", "камень", "ножницы", "бумага"]
+words = ["", "камень", "ножницы", "бумага"]
 
 
 def find_winner(first, second):
-    summ = words.index(first) - words.index(second)
-    if -2 < summ < 0 or summ == 2:
-        return "игрок 1 выиграл"
-    elif 2 > summ > 0 or summ == -2:
-        return "игрок 2 выиграл"
-    else:
-        return
+    if first in words and second in words:
+        summ = words.index(first) - words.index(second)
+        if -2 < summ < 0 or summ == 2:
+            return "игрок 1 выиграл"
+        elif 2 > summ > 0 or summ == -2:
+            return "игрок 2 выиграл"
+        else:
+            return "ничья"
+    return "вы проиграли - для игры нужны лишь: камень, ножницы, бумага"
 
 
 print(find_winner(first_player, second_player))
