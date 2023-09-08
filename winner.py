fe6d6d523f03577234d06999e0dd6a84c6a9735f
predeludel <sinitsa.2003@yandex.ru>
2023-09-08 11:57:36 +0300

    correction of assignments

--- winner.py
+++ winner.py
@@ -1,25 +1,44 @@
 from import_this import RACE_DATA
 import datetime
+from typing import TypeAlias
 
 RACE_DATA = RACE_DATA
-print(RACE_DATA)
-winners = {}
-for team in RACE_DATA:
-    value = RACE_DATA.get(team)
-    for place in value:
-        if value.get('FinishedPlace') in [1, 2, 3]:
-            winners.update({value.get('FinishedPlace'):
-                                value})
-            break
-wine = winners.get(1)
-print(f'Выиграл - {wine.get("RacerName")}!!! Поздравляем!!')
-print(f'_______________________________')
-print(f'Первые три места:\n')
-for i in dict(sorted(winners.items())):
-    value = winners.get(i)
-    time = str(datetime.timedelta(seconds=int(value.get('FinishedTimeSeconds'))))
+FinishedPlace: TypeAlias = int
+FinishedTimeSeconds: TypeAlias = str | int | float
+RacerName: TypeAlias = str
+RacerTeam: TypeAlias = str
 
-    print(f'Гонщик на {i} метсте')
-    print(f"\t\tИмя: {value.get('RacerName')}")
-    print(f"\t\tКоманда: {value.get('RacerTeam')}")
-    print(f"\t\tВремя: {time} (H:M:S)\n ")
+RacerInfo: TypeAlias = dict[
+    str, FinishedPlace | FinishedTimeSeconds | RacerName | RacerTeam
+]
+WINNERS: dict[int, RacerInfo] = {}
+
+
+def get_array(race_data) -> dict:
+    for team in race_data:
+        value = race_data.get(team)
+        for i in value:
+            if value.get('FinishedPlace') in [1, 2, 3]:
+                WINNERS.update({value.get('FinishedPlace'): value})
+                break
+    return WINNERS
+
+
+def get_text():
+    wine = WINNERS.get(1)
+    print(f'Выиграл - {wine.get("RacerName")}!!! Поздравляем!!')
+    print(f'_______________________________')
+    print(f'Первые три места:\n')
+    for i in dict(sorted(WINNERS.items())):
+        value = WINNERS.get(i)
+        time = str(datetime.timedelta(seconds=int(value.get('FinishedTimeSeconds'))))
+
+        print(f'Гонщик на {i} метсте')
+        print(f"\t\tИмя: {value.get('RacerName')}")
+        print(f"\t\tКоманда: {value.get('RacerTeam')}")
+        print(f"\t\tВремя: {time} (H:M:S)\n ")
+
+
+if __name__ == '__main__':
+    get_array(RACE_DATA)
+    get_text()
